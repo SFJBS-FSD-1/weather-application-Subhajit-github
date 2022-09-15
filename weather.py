d8bf0efd19c1b17fce5ac972011d487399387dc5
Subhajit-github <subhajitbhattacharya08@gmail.com>
2022-09-15 09:05:54 +0000

    Pushing next version

--- weather.py
+++ weather.py
@@ -1,10 +1,7 @@
-import json
 import requests
 import datetime
 import os
-
 from flask import Flask, render_template, request
-import urllib.request
 
 app = Flask(__name__)  #creating a flask instance
 
@@ -16,11 +13,7 @@ def weather():
         apiKey= "8fe6d1156da5eeada955de2c33be8cbe"
         unit = "metric"
         url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" +apiKey+ "&units=" + unit + ""
-        print(url)
         response = requests.get(url).json()
-        # icon = response["weather"][0]["icon"]
-        # image_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-        print(response["cod"])
         if response["cod"] == 200:
             icon = response["weather"][0]["icon"]
             image_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
@@ -32,10 +25,9 @@ def weather():
                "sunrise": datetime.datetime.fromtimestamp(response.get('sys')['sunrise']),
                "sunset": datetime.datetime.fromtimestamp(response.get('sys')['sunset']),
                "status": 200}
-            print(data)
             return render_template("index.html", my_name="Subhajit", data=data)
         elif response["cod"] == "404":
-            data = {"message": "City Invalid", "status": 404 }
+            data = {"message": "This city does not exist. Please enter correct city and try again!", "status": 404 }
             return render_template("index.html", my_name="Subhajit", data=data)
         else:
             data = {"message": response["message"], "status": response["cod"]}
